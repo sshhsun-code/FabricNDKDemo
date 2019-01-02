1d18968f8ca0e0129ba7c98c1eb1ec42cc408de6
sunqi <iamsshhsun@gamil.com>
2019-01-02 16:15:14 +0800

    添加Fabric 崩溃分析支持

--- app/src/main/java/demo/sunqi13/com/fabricndkdemo/MainActivity.java
+++ app/src/main/java/demo/sunqi13/com/fabricndkdemo/MainActivity.java
@@ -3,6 +3,8 @@ package demo.sunqi13.com.fabricndkdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
